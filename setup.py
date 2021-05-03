7d3db41727c1257b509f60c7a8617aabd45ae326
Steve Ayers <californiastephen@outlook.com>
2021-05-03 13:41:53 -0700

    pypi upload

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     # Needed for dependencies
     # install_requires=[''],
     # *strongly* suggested for sharing
-    version='1.0.6',
+    version='1.0.7',
     # The license can be anything you like
     license='MIT',
     # long_description=open('README.md').read(),
