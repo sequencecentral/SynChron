4b806889592926f49f1d37dee2819ff265249fdb
Steve Ayers <californiasteven@outlook.com>
2021-08-11 14:16:21 -0700

    pypi upload

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     author='Steve Ayers, Ph.D.',
     author_email='steve@sequenccecentral.com',
     # install_requires=[],
-    version='1.2.3',
+    version=open('VERSION').read(),
     license='MIT',
     # packages=['synchronicity','synchronicity.quotewidget'],
     packages = find_packages(),
